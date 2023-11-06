de1608d7929588f63ca35c1b78439630837bb289
Muhammad Alamgir Khan <98054130+aalamkh@users.noreply.github.com>
2023-11-06 13:52:59 -0800

    Time: 45 ms (93.11%), Space: 42.3 MB (38.78%) - LeetHub

---
+++ 0274-h-index/0274-h-index.js
@@ -0,0 +1,14 @@
+var hIndex = function(citations) {
+    citations.sort((a, b) => b - a); // Sort the citations in descending order.
+    let h = 0;
+    
+    for (let i = 0; i < citations.length; i++) {
+        if (citations[i] >= i + 1) {
+            h = i + 1; // Update the H-Index if the condition is met.
+        } else {
+            break; // Stop if the condition is no longer met because of the descending sort.
+        }
+    }
+    
+    return h;
+};
