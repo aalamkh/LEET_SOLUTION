939a4bc634b3bb00138ce37bda045fe08f101030
Muhammad Alamgir Khan <98054130+aalamkh@users.noreply.github.com>
2023-11-22 00:15:21 -0800

    palindrome-number.js

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,18 +2,10 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function (x) {
-  let riminder = 0;
-  let reverse = 0;
-  let number = x;
-  if (x < 0) {
-    return false;
-  }
-
-  while (number !== 0) {
-    riminder = number % 10;
-    reverse = reverse * 10 + riminder;
-    number = Math.floor(number / 10);
-  }
-  return x === reverse;
-};
+var isPalindrome = function(x) {
+   if(x<0){
+     return false
+   }else{
+  return x == x.toString().split("").reverse().join("")
+   }
+}
