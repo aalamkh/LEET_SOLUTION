1deded33e4c17f13ec8d1532132719f532886e25
Muhammad Alamgir Khan <98054130+aalamkh@users.noreply.github.com>
2023-11-25 01:01:10 -0800

    Update 0021-merge-two-sorted-lists.js

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -1,5 +1,5 @@
 var mergeTwoLists = function (l1, l2) {
-    if (!l1) return l2;
+    if (!l1) return l2; 
     else if (!l2) return l1;
     else if (l1.val <= l2.val) {
         l1.next = mergeTwoLists(l1.next, l2) ;
