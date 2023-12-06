0036b774462f53fe0d1a96b24f5812cdf636d634
Muhammad Alamgir Khan <98054130+aalamkh@users.noreply.github.com>
2023-12-06 08:33:22 -0800

    Update 0274-h-index.js

--- 0274-h-index/0274-h-index.js
+++ 0274-h-index/0274-h-index.js
@@ -1,6 +1,6 @@
 var hIndex = function(citations) {
     citations.sort((a, b) => b - a); // Sort the citations in descending order.
-    let h = 0;
+    let h = 0; 
     
     for (let i = 0; i < citations.length; i++) {
         if (citations[i] >= i + 1) {
