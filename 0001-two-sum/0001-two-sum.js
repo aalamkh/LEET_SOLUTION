77ded5df273eb7a3281b34794cac89d3cc33b75e
Muhammad Alamgir Khan <98054130+aalamkh@users.noreply.github.com>
2023-12-15 15:34:38 +0000

    Update 0001-two-sum.js

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -13,7 +13,7 @@ var twoSum = function(nums, target) {
       
     for(let i = 0; i < nums.length; i ++) { 
         if(map.has(target - nums[i])) { 
-            return [map.get(target - nums[i]), i];
+            return [map.get(target - nums[i]), i]; 
         } else {
             map.set(nums[i], i);
         }
